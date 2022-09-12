6c9d0e9cbf0d4e43dfafc3ebb772458ec5066d97
BilalAlnajjar <bilalalnajjar46@gmail.com>
2022-09-12 08:44:07 +0300

    first commit

---
+++ src/_nav.js
@@ -0,0 +1,273 @@
+import React from 'react'
+import CIcon from '@coreui/icons-react'
+import {
+  cilBell,
+  cilCalculator,
+  cilChartPie,
+  cilCursor,
+  cilDescription,
+  cilDrop,
+  cilNotes,
+  cilPencil,
+  cilPuzzle,
+  cilSpeedometer,
+  cilStar,
+} from '@coreui/icons'
+import { CNavGroup, CNavItem, CNavTitle } from '@coreui/react'
+
+const _nav = [
+  // {
+  //   component: CNavItem,
+  //   name: 'Dashboard',
+  //   to: '/dashboard',
+  //   icon: <CIcon icon={cilSpeedometer} customClassName="nav-icon" />,
+  //   badge: {
+  //     color: 'info',
+  //     text: 'NEW',
+  //   },
+  // },
+  // {
+  //   component: CNavTitle,
+  //   name: 'Theme',
+  // },
+  // {
+  //   component: CNavItem,
+  //   name: 'Subjects',
+  //   to: '/theme/colors',
+  //   icon: <CIcon icon={cilDrop} customClassName="nav-icon" />,
+  // },
+  // {
+  //   component: CNavItem,
+  //   name: 'General Plan',
+  //   to: '/theme/typography',
+  //   icon: <CIcon icon={cilPencil} customClassName="nav-icon" />,
+  // },
+  //
+  // {
+  //   component: CNavItem,
+  //   name: 'My Plans',
+  //   to: '/theme/typography',
+  //   icon: <CIcon icon={cilPencil} customClassName="nav-icon" />,
+  // },
+  //
+  // {
+  //   component: CNavItem,
+  //   name: 'Helps',
+  //   to: '/theme/typography',
+  //   icon: <CIcon icon={cilPencil} customClassName="nav-icon" />,
+  // },
+  //
+  // {
+  //   component: CNavTitle,
+  //   name: 'Components',
+  // },
+  {
+    component: CNavGroup,
+    name: 'Base',
+    to: '/base',
+    icon: <CIcon icon={cilPuzzle} customClassName="nav-icon" />,
+    items: [
+      {
+        component: CNavItem,
+        name: 'My plans',
+        to: '/base/accordion',
+      },
+      {
+        component: CNavItem,
+        name: 'Help',
+        to: '/base/breadcrumbs',
+      },
+      {
+        component: CNavItem,
+        name: 'Cards',
+        to: '/base/cards',
+      },
+      {
+        component: CNavItem,
+        name: 'Carousel',
+        to: '/base/carousels',
+      },
+      {
+        component: CNavItem,
+        name: 'Collapse',
+        to: '/base/collapses',
+      },
+      {
+        component: CNavItem,
+        name: 'List group',
+        to: '/base/list-groups',
+      },
+      {
+        component: CNavItem,
+        name: 'Navs & Tabs',
+        to: '/base/navs',
+      },
+      {
+        component: CNavItem,
+        name: 'Pagination',
+        to: '/base/paginations',
+      },
+      {
+        component: CNavItem,
+        name: 'Placeholders',
+        to: '/base/placeholders',
+      },
+      {
+        component: CNavItem,
+        name: 'Popovers',
+        to: '/base/popovers',
+      },
+      {
+        component: CNavItem,
+        name: 'Progress',
+        to: '/base/progress',
+      },
+      {
+        component: CNavItem,
+        name: 'Spinners',
+        to: '/base/spinners',
+      },
+      {
+        component: CNavItem,
+        name: 'Tables',
+        to: '/base/tables',
+      },
+      {
+        component: CNavItem,
+        name: 'Tooltips',
+        to: '/base/tooltips',
+      },
+    ],
+  },
+
+  {
+    component: CNavGroup,
+    name: 'Subjects',
+    to: '/base',
+    icon: <CIcon icon={cilPuzzle} customClassName="nav-icon" />,
+    items: [
+      {
+        component: CNavItem,
+        name: 'My plans',
+        to: '/base/accordion',
+      },
+      {
+        component: CNavItem,
+        name: 'Help',
+        to: '/base/breadcrumbs',
+      },
+      {
+        component: CNavItem,
+        name: 'Cards',
+        to: '/base/cards',
+      },
+    ],
+  },
+
+  {
+    component: CNavGroup,
+    name: 'General Plan',
+    to: '/base',
+    icon: <CIcon icon={cilPuzzle} customClassName="nav-icon" />,
+    items: [
+      {
+        component: CNavItem,
+        name: 'My plans',
+        to: '/base/accordion',
+      },
+      {
+        component: CNavItem,
+        name: 'Help',
+        to: '/base/breadcrumbs',
+      },
+      {
+        component: CNavItem,
+        name: 'Cards',
+        to: '/base/cards',
+      },
+      {
+        component: CNavItem,
+        name: 'Carousel',
+        to: '/base/carousels',
+      },
+      {
+        component: CNavItem,
+        name: 'Collapse',
+        to: '/base/collapses',
+      },
+    ],
+  },
+
+  {
+    component: CNavGroup,
+    name: 'My plans',
+    to: '/base',
+    icon: <CIcon icon={cilPuzzle} customClassName="nav-icon" />,
+    items: [
+      {
+        component: CNavItem,
+        name: 'My plans',
+        to: '/base/accordion',
+      },
+      {
+        component: CNavItem,
+        name: 'Help',
+        to: '/base/breadcrumbs',
+      },
+      {
+        component: CNavItem,
+        name: 'Cards',
+        to: '/base/cards',
+      },
+      {
+        component: CNavItem,
+        name: 'Carousel',
+        to: '/base/carousels',
+      },
+      {
+        component: CNavItem,
+        name: 'Collapse',
+        to: '/base/collapses',
+      },
+      {
+        component: CNavItem,
+        name: 'List group',
+        to: '/base/list-groups',
+      },
+      {
+        component: CNavItem,
+        name: 'Navs & Tabs',
+        to: '/base/navs',
+      },
+      {
+        component: CNavItem,
+        name: 'Pagination',
+        to: '/base/paginations',
+      },
+    ],
+  },
+  {
+    component: CNavGroup,
+    name: 'Help',
+    to: '/base',
+    icon: <CIcon icon={cilPuzzle} customClassName="nav-icon" />,
+    items: [
+      {
+        component: CNavItem,
+        name: 'My plans',
+        to: '/base/accordion',
+      },
+      {
+        component: CNavItem,
+        name: 'Help',
+        to: '/base/breadcrumbs',
+      },
+      {
+        component: CNavItem,
+        name: 'Cards',
+        to: '/base/cards',
+      },
+    ],
+  },
+]
+export default _nav
